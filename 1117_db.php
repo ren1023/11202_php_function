983f9d85be19295d25473c07453c8cd34c5f6de5
ren1023 <renyou.1023@gmail.com>
2023-11-17 16:26:46 +0800

    feat:增加資料庫多筆查詢

--- 1117_db.php
+++ 1117_db.php
@@ -1,6 +1,6 @@
 <?php
 // $rows=all();
-$rows = dball('dept');
+$rows = dball('students', ['dept'=>'1']);
 dd($rows);
 function all()
 {
@@ -8,6 +8,7 @@ function all()
     $pdo = new PDO($dsn, 'root', '');
     $sql = "select * from `students`";
     $row = $pdo->query($sql)->fetchAll();
+   
     return $row;
 }
 function dd($array)
@@ -16,15 +17,27 @@ function dd($array)
     print_r($array);
     echo "<pre>";
 }
-function dball($tname = null)
-{
+function dball($table = null, $where = ''){
+    $sql="select * from `$table` ";
     $dsn = "mysql:localhost;charset=utf8;dbname=school";
     $pdo = new PDO($dsn, 'root', '');
-    if (isset($tname) && !empty($tname)) {
-        $sql = "select * from `$tname`";
-        $row = $pdo->query($sql)->fetchAll();
+    if (isset($table) && !empty($table)) {
+        if (is_array($where)) {
+            if (!empty($where)) {
+                foreach ($where as $col => $value) {
+                    echo $col . ": " . $value . "<br>";
+                    $tmp[] = "`$col`='$value'";
+                }
+                $sql.=" where ".join(" && ",$tmp);
+            }  else {
+            $sql .= " $where";
+        }
+        // echo $sql;
+        // $row = $pdo->query($sql)->fetchAll();
+        $row = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
         return $row;
     } else {
         echo "請輸入正確認資料表名稱";
     }
+}
 }
