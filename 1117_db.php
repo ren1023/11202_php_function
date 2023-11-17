c04fffc7d3c2f5d39f0e8143d13cca7fb8892067
ren1023 <renyou.1023@gmail.com>
2023-11-17 16:35:09 +0800

    feat:移動程式碼

--- 1117_db.php
+++ 1117_db.php
@@ -1,23 +1,21 @@
 <?php
 // $rows=all();
-$rows = dball('students', ['dept'=>'1']);
+// $rows = dball('students', ['dept'=>'1'],'order by id desc');
+// $rows = dball('students','order by id desc');
+$rows=dball('students',['dept'=>'3']);
 dd($rows);
-function all()
-{
-    $dsn = "mysql:localhost;charset=utf8;dbname=school";
-    $pdo = new PDO($dsn, 'root', '');
-    $sql = "select * from `students`";
-    $row = $pdo->query($sql)->fetchAll();
+// function all()
+// {
+//     $dsn = "mysql:localhost;charset=utf8;dbname=school";
+//     $pdo = new PDO($dsn, 'root', '');
+//     $sql = "select * from `students`";
+//     $row = $pdo->query($sql)->fetchAll();
    
-    return $row;
-}
-function dd($array)
-{
-    echo "<pre>";
-    print_r($array);
-    echo "<pre>";
-}
-function dball($table = null, $where = ''){
+//     return $row;
+// }
+
+
+function dball($table = null, $where = '',$other=''){
     $sql="select * from `$table` ";
     $dsn = "mysql:localhost;charset=utf8;dbname=school";
     $pdo = new PDO($dsn, 'root', '');
@@ -25,19 +23,28 @@ function dball($table = null, $where = ''){
         if (is_array($where)) {
             if (!empty($where)) {
                 foreach ($where as $col => $value) {
-                    echo $col . ": " . $value . "<br>";
+                    // echo $col . ": " . $value . "<br>";
                     $tmp[] = "`$col`='$value'";
                 }
                 $sql.=" where ".join(" && ",$tmp);
             }  else {
             $sql .= " $where";
         }
+
+        $sql.=$other;
         // echo $sql;
         // $row = $pdo->query($sql)->fetchAll();
-        $row = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
-        return $row;
+        $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        return $rows;
     } else {
         echo "請輸入正確認資料表名稱";
     }
 }
 }
+function dd($array)
+{
+    echo "<pre>";
+    print_r($array);
+    echo "<pre>";
+}
+?>
